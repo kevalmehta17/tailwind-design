92bdaebb59a102ff4e9c2d5e240c6b3920ca28dc
keval <kevalemhta885@gmail.com>
2026-03-16 10:24:25 +0530

    initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="bg-gray-500 min-h-screen">
+      <h1 className="flex bg-yellow-200 text-orange-400">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
